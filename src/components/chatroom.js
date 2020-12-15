c9a9de8ec2c4465fb19232d806481064dcfa06f9
Ehsan ul haq khawja <uehsan3@icloud.com>
2020-12-15 21:39:31 +0530

    Fixed spam cause & restricted collection length

--- src/components/chatroom.js
+++ src/components/chatroom.js
@@ -1,31 +1,29 @@
 import React, { useRef, useState } from "react";
 import firebase from "firebase/app";
 import ChatMessage from "./chatmessage.js";
-
 import { useCollectionData } from "react-firebase-hooks/firestore";
-
-// let gDummy;
-// window.onload = function () {
-//   setInterval(gDummy.current.scrollIntoView(), 1000);
-// };
-let never = 1;
 
 function ChatRoom(props) {
   const { firestore, auth } = props;
   const dummy = useRef();
-  // gDummy = dummy;
   const messagesRef = firestore.collection("messages");
   const query = messagesRef.orderBy("createdAt", "asc");
   const [messages] = useCollectionData(query, { idField: "id" });
-
   const [formValue, setFormValue] = useState("");
 
   const sendMessage = async (e) => {
     //normally when a form is submitted it refreshes the page, but this prevents it
+
     e.preventDefault();
 
-    const { uid, photoURL } = auth.currentUser;
+    //prevents user from spamming empty messages which increases the
+    //total reads from database also trims down the uneccesary whitespaces
 
+    if (!formValue.trim()) return;
+
+    //adds data to the database
+
+    const { uid, photoURL } = auth.currentUser;
     await messagesRef.add({
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
@@ -33,14 +31,29 @@ function ChatRoom(props) {
       photoURL,
     });
 
+    //refreshes formvalue to empty
+
     setFormValue("");
+
+    //deletes the firestore documents when collectoin length exceeds the limit
+    //in order to reduce total reads afterwards writes and deletes go together
+
+    let len = messages.length;
+    let i = 0;
+    while (len > 20) {
+      firestore.collection("messages").doc(messages[i].id).delete();
+      i++;
+      len--;
+    }
+
+    //scrolls the page to bottom to see the typed new message
 
     dummy.current.scrollIntoView();
   };
 
   return (
     <>
-      <main onChange={runThis()}>
+      <main>
         <div>
           {messages &&
             messages.map((msg) => (
@@ -58,15 +71,6 @@ function ChatRoom(props) {
       </form>
     </>
   );
-}
-
-function runThis() {
-  if (never) {
-    setInterval(() => {
-      document.getElementById("hhh").scrollIntoView();
-    }, 1000);
-    never = 0;
-  }
 }
 
 export default ChatRoom;
